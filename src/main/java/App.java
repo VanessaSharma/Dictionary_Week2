d3d854ef3371dbc126f4800150ac5579a1d119ff
Vanessa Palacios <vanelunapalacios@live.com>
2016-09-16 08:18:00 -0700

    Add code for .gitignore, VelocityTemplateEngine.vtl, and build.gradle.

---
+++ src/main/java/App.java
@@ -0,0 +1,21 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayList;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+import java.util.Collections;
+
+public class App{
+  public static void main(String[] args){
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+
+
+
+
+
+
+  }
+}
